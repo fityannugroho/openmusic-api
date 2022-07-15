76d4f03224b40bac811212121081e0fe17c562b8
Fityan <fityannugroho@gmail.com>
2022-07-15 20:21:25 +0700

    Install `@hapi/hapi` and init the server
    - Install `@hapi/hapi@20.2.2`.
    - Initialize the server in `./src/index.js`.
    - Create server config variables: `HOST` and `PORT`.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  // TODO: Register the plugins
+  // ...
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+init();
