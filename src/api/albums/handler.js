48fc8526a1f19a1276cc04a8975c718be39cdf18
Fityan <fityannugroho@gmail.com>
2022-07-17 16:25:05 +0700

    Create albums handler

---
+++ src/api/albums/handler.js
@@ -0,0 +1,111 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class AlbumsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+  }
+
+  async postAlbumHandler(request, h) {
+    try {
+      this._validator.validateAlbumPayload(request.payload);
+
+      const { name, year } = request.payload;
+      const albumId = await this._service.addAlbum({ name, year });
+
+      return h.response({
+        status: 'success',
+        data: { albumId },
+      }).code(201);
+    } catch (error) {
+      if (error instanceof ClientError) {
+        return h.response({
+          status: 'fail',
+          message: error.message,
+        }).code(error.statusCode);
+      }
+
+      return h.response({
+        status: 'error',
+        message: 'Something went wrong in our server.',
+      }).code(500);
+    }
+  }
+
+  async getAlbumByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      const album = await this._service.getAlbumById(id);
+
+      return h.response({
+        status: 'success',
+        data: { album },
+      });
+    } catch (error) {
+      if (error instanceof ClientError) {
+        return h.response({
+          status: 'fail',
+          message: error.message,
+        }).code(error.statusCode);
+      }
+
+      return h.response({
+        status: 'error',
+        message: 'Something went wrong in our server.',
+      }).code(500);
+    }
+  }
+
+  async putAlbumByIdHandler(request, h) {
+    try {
+      this._validator.validateAlbumPayload(request.payload);
+
+      const { id } = request.params;
+      const { name, year } = request.payload;
+      await this._service.editAlbumById(id, { name, year });
+
+      return h.response({
+        status: 'success',
+        message: 'Album was updated.',
+      });
+    } catch (error) {
+      if (error instanceof ClientError) {
+        return h.response({
+          status: 'fail',
+          message: error.message,
+        }).code(error.statusCode);
+      }
+
+      return h.response({
+        status: 'error',
+        message: 'Something went wrong in our server.',
+      }).code(500);
+    }
+  }
+
+  async deleteAlbumByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      await this._service.deleteAlbumById(id);
+
+      return h.response({
+        status: 'success',
+        message: 'Album was deleted.',
+      });
+    } catch (error) {
+      if (error instanceof ClientError) {
+        return h.response({
+          status: 'fail',
+          message: error.message,
+        }).code(error.statusCode);
+      }
+
+      return h.response({
+        status: 'error',
+        message: 'Something went wrong in our server.',
+      }).code(500);
+    }
+  }
+}
+
+module.exports = AlbumsHandler;
