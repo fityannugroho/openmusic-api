f944fe1cef922957452b387f0dbf3a984f4bc743
Fityan <fityannugroho@gmail.com>
2023-05-20 20:10:45 +0700

    feat(migrations): create table `users` and `authentications`

---
+++ migrations/1684587064597_create-table-users.js
@@ -0,0 +1,29 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(21)',
+      primaryKey: true,
+    },
+    username: {
+      type: 'VARCHAR(50)',
+      unique: true,
+      notNull: true,
+    },
+    password: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    fullname: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('users');
+};
