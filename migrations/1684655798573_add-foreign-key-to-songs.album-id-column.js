72b43ba73ac27adaab11aa9063df7371b56b8b49
Fityan <fityannugroho@gmail.com>
2023-05-21 20:10:49 +0700

    fix(songs): change default value for albumId Change from `''` to `null`

--- migrations/1684655798573_add-foreign-key-to-songs.album-id-column.js
+++ migrations/1684655798573_add-foreign-key-to-songs.album-id-column.js
@@ -5,7 +5,7 @@ exports.shorthands = undefined;
 exports.up = (pgm) => {
   pgm.sql("INSERT INTO albums VALUES ('old_songs', 'old_songs', 2023)");
 
-  pgm.sql("UPDATE songs SET album_id = 'old_songs' WHERE album_id = ''");
+  pgm.sql("UPDATE songs SET album_id = 'old_songs' WHERE album_id = NULL");
 
   pgm.addConstraint('songs', 'fk_songs.album_id_albums.id', 'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE SET NULL');
 };
@@ -13,7 +13,7 @@ exports.up = (pgm) => {
 exports.down = (pgm) => {
   pgm.dropConstraint('songs', 'fk_songs.album_id_albums.id');
 
-  pgm.sql("UPDATE songs SET album_id = '' WHERE album_id = 'old_songs'");
+  pgm.sql("UPDATE songs SET album_id = NULL WHERE album_id = 'old_songs'");
 
   pgm.sql("DELETE FROM albums WHERE id = 'old_songs'");
 };
