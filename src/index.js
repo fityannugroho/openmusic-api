3e8be8a0cf9e28588945a9842b886b8b3afd4973
Fityan <fityannugroho@gmail.com>
2023-05-24 16:07:03 +0700

    feat(albums): implement caching for album liking feature

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ const ExportsValidator = require('./validator/exports');
 const config = require('./utils/config');
 const LocalStorageService = require('./services/storage/local/LocalStorageService');
 const _files = require('./api/files');
+const CacheService = require('./services/redis/CacheService');
 
 const init = async () => {
   const server = Hapi.server({
@@ -69,9 +70,10 @@ const init = async () => {
     }),
   });
 
+  const cacheService = new CacheService();
   const storageService = new LocalStorageService(path.resolve(__dirname, 'storage'));
   const songsService = new SongsService();
-  const albumsService = new AlbumsService(songsService);
+  const albumsService = new AlbumsService(songsService, cacheService);
   const usersService = new UsersService();
   const authService = new AuthService();
   const collaborationsService = new CollaborationsService();
